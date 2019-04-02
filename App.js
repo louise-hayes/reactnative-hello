e80cab2625d9339de80b3653618863ca75195412
Louise Hayes <louise@deewhy.ie>
2019-04-02 12:17:41 -0400

    homepage with fetch to github

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, NavigatorIOS} from 'react-native';
+import SearchComponent from './app/searchcomponent';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <NavigatorIOS
+      style={styles.container}
+      initialRoute = {{
+       title: 'Fetch Example',
+       component: SearchComponent
+     }} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#111111',
+  }
+});
